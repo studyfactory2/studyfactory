531ddfa6b7616af612b4c9af167e9e807404a825
hwan2 <hwan2@hwan2ui-Macmini.local>
2026-06-18 09:50:42 +0900

    Initial commit

---
+++ src/main/java/com/example/studyfactory/StudyfactoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyfactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyfactoryApplication.class, args);
+    }
+
+}
